f99ef61a4e7796ae6a71d901a36f84b31f46cdbd
Luana <luana.mouro@gmail.com>
2022-07-30 10:39:28 -0300

    Commit Alteracoes data

--- app/src/main/java/me/dio/business_card/data/BusinessCardRepository.kt
+++ app/src/main/java/me/dio/business_card/data/BusinessCardRepository.kt
@@ -1,4 +1,16 @@
 package me.dio.business_card.data
 
-class BusinessCardRepository {
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
+class BusinessCardRepository(private val dao: BusinessCardDao) {
+    fun insert(businessCard: BusinessCard) = runBlocking {
+        launch(Dispatchers.IO) {
+            dao.insert(businessCard)
+        }
+    }
+
+    fun getAll() = dao.getAll()
 }
