c36dc2ca41904c75d8f68c15b5312dff0cbfcb70
Luana <luana.mouro@gmail.com>
2022-07-30 10:15:17 -0300

    Alteracoes R01

--- app/src/main/java/me/dio/business_card/ui/AddBusinessCardActivity.kt
+++ app/src/main/java/me/dio/business_card/ui/AddBusinessCardActivity.kt
@@ -2,10 +2,29 @@ package me.dio.business_card.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View.inflate
+
 
 class AddBusinessCardActivity : AppCompatActivity() {
+    private val binding by lazy {ActivityAddBusinessCardBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_business_card)
+        setContentView(binding.root)
+        insertListeners()
     }
+
+    private fun insertListeners(){
+        binding.ibClose.setOnClickListener{
+            finish()
+        }
+
+        binding.btnConfirm.setOnClickListener{
+
+        }
+    }
+}
+
+class ActivityAddBusinessCardBinding {
+
 }
