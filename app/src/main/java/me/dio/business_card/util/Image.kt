149047a22add126728be5aac81b13b5d936a1108
Luana <luana.mouro@gmail.com>
2022-07-30 14:05:34 -0300

    Commit video10 R01

--- app/src/main/java/me/dio/business_card/util/Image.kt
+++ app/src/main/java/me/dio/business_card/util/Image.kt
@@ -1,10 +1,32 @@
 package me.dio.business_card.util
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
 import android.view.View
 
 class Image {
     fun share(context: Context, view: View){
-        val bitmap = getScreenShotFromView()
+        val bitmap = getScreenShotFromView(view)
+
+        bitmap?.let{
+            saveMediaToStorage()
+        }
+    }
+
+    private fun getScreenShotFromView(card: View): Bitmap? {
+        var screenshot: Bitmap? = null
+        try{
+            screenshot = Bitmap.createBitmap(
+                card.measuredWidth,
+                card.measuredHeight,
+                Bitmap.Config.ARGB_8888
+            )
+
+            val canvas: Canvas(screenshot)
+            card.draw(canvas)
+        }catch (e: Exception){
+            log.e("Error ->", "Falha ao capturar imagem.")
+        }
     }
 }
